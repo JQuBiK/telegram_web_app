1cddb33b97e5cdec75451c0b54c742c60b49356a
Igor Rozhkov <freezof1x_1103@mai.ru>
2024-05-17 09:21:27 +0300

    added few changes to header

--- src/companents/header/header.js
+++ src/companents/header/header.js
@@ -5,15 +5,11 @@ const Header = () => {
         <header className="header">
             <ul className="header-list">
                 <li className="header-list-item">
-                    <button className='header-get-info-btn'>
-                        <img src={require("./img/info.png")} alt="info" />
-                    </button>
+                    <button className='header-get-info-btn'></button>
                 </li>
-                <li className="header-list-item">PROJECTS</li>
+                <li className="header-list-item"><h1>PROJECTS</h1></li>
                 <li className="header-list-item">
-                    <button className='header-get-info-btn'>
-                        <img src={require("./img/notification.png")} alt="info" />
-                    </button>
+                    <button className='header-notification-btn'></button>
                 </li>
             </ul>
         </header>
